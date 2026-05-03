9a4a116c09a623ce0ff6dec2d831e9e4acd9314b
Li Jin <punkid.online@gmail.com>
2026-05-03 14:14:12 +0800

    add wxss and wxml filetype treesitter support, removed sidekick plugin

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.mdx",
+  callback = function()
+    local date = os.date("%Y-%m-%d")
+
+    local lines = {
+      "---",
+      'title: " "',
+      "date: " .. date,
+      "categories:",
+      '  - " "',
+      "tags:",
+      '  - " "',
+      "---",
+      "",
+    }
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  end,
+})
